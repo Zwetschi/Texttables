ea4deb7ff2be6df6228a9c7c4663be1d3c85a971
Philipp Gogolin <gogolin@hm.edu>
2023-10-06 09:06:32 +0200

    Break, cell wirter - its to hard to write cells where ever i want. i think the parser should work but the Input gets to wired

--- Texttables_3.py
+++ Texttables_3.py
@@ -8,7 +8,7 @@ class _CellTextDistance(NamedTuple):
     sum: int
 
 
-class InputCell:
+class CellParser:
     """
     Check and modiefie the input
     ┌──────────────────────┬────────────────────────┐
@@ -17,7 +17,50 @@ class InputCell:
     └──────────────────────┴────────────────────────┘
     """
 
-    def __init__(self, text: str, *args, **kwargs) -> None:
+    def __init__(
+        self, text: str, hight: int, width: int = "auto", *args, **kwargs
+    ) -> None:
+        self._init_chars_and_align()
+        self._cell_text_original_input = self.__check_cell_input(text).split("\n")
+        self._cell_text = []  # modiefied data with valign
+        self.args = args
+        self.kwargs = kwargs
+        self._set_width(width)
+        self._hight = hight
+        self.set_cell_text_to_border()
+
+        # register iterator. (iter starting new when iterator() is called)
+        self.get = self._cell_parser()
+
+    def get_line(self, position: int = None):
+        if position is None:
+            return self._cell_text
+        return self._cell_text[position]
+
+    def set_cell_text_to_border(self, left: str = " ", right: str = " "):
+        """set the distance to the frame on the text in echt cell\n
+        left = Y = " " \n
+        right = X = " " \n
+        ┌────────────────────┐\n
+        │Y               oneX│\n
+        │Y               oneX│\n
+        └────────────────────┘\n
+        """
+        if not isinstance(left, str) or not isinstance(left, str):
+            raise ValueError(f"left and right must be string!")
+
+        self._distance_border_text = _CellTextDistance(
+            left, right, len(left) + len(right)
+        )
+        self._set_cell_width_auto()  # callback
+
+    def _set_width(self, width):
+        if width == "auto":
+            self._do_auto_width = True
+        else:
+            self._do_auto_width = False
+
+    def _init_chars_and_align(self):
         self._corner_top_left = ""
         self._corner_top_right = "+"
         self._corner_bottom_left = ""
@@ -26,51 +69,42 @@ class InputCell:
         self._delimiter_bottom = "-"
         self._delimeter_left = ""
         self._delimeter_right = "|"
-        self._width = 20
-        self._hight = 1
-        self._align = "l"
+        self._align = "c"
         self._valign = "t"
-        self._newline = "\n"
+        self._newline = ""
 
-        self.args = args
-        self.kwargs = kwargs
-        self._cell_text = []  # modiefied with valign
-        # originial input
-        self._cell_text_input = self.__check_cell_input(text).split("\n")
-        self.set_cell_text_to_border()
-
-    def get_line(self, position: int = None):
-        if position is None:
-            return self._cell_text
-        return self._cell_text[position]
+    def _set_cell_width_auto(self):
+        if self._do_auto_width:
+            line_widths = [len(line) for line in self._cell_text_original_input]
+            self._width = max(line_widths) + self._distance_border_text.sum
 
     def _cell_parser(self):
-        self.__parse_1_sort()
-        yield self.__parse_2_top_line()
+        self._parse_1_sort()
+        yield self._parse_2_top_line()
 
         for text in self._cell_text:
-            yield self.__parse_3_text_cell(text)
-        yield self.__parse_4_bottom_line()
+            yield self._parse_3_text_cell(text)
+        yield self._parse_4_bottom_line()
 
-    def __parse_1_sort(self):
+    def _parse_1_sort(self):
         """sort the list
 
         Raises:
             Exception: _description_
         """
-        if len(self._cell_text_input) < self._hight:
-            raise (
-                f"given cell hight in lines is smaler then given data. {self._cell_text_input}"
+        if len(self._cell_text_original_input) > self._hight:
+            raise Exception(
+                f"given cell hight in lines is smaler then given data. {self._cell_text_original_input}"
             )
-        self._cell_text = self._cell_text_input.copy()
-        diff = self._hight - len(self._cell_text_input)
+        self._cell_text = self._cell_text_original_input.copy()
+        diff = self._hight - len(self._cell_text_original_input)
         if self._valign in ("t", "top"):
             for i in range(diff):
                 self._cell_text.append("")
-        elif self._valign == ("b", "bottom"):
+        elif self._valign in ("b", "bottom"):
             for i in range(diff):
                 self._cell_text.insert(0, "")
-        elif self._valign == ("m", "middle"):
+        elif self._valign in ("m", "middle"):
             center_top = diff // 2
             center_bottom = (diff + 1) // 2
             for k in range(center_bottom):
@@ -78,9 +112,9 @@ class InputCell:
             for j in range(center_top):
                 self._cell_text.insert(0, "")
         else:
-            raise Exception
+            raise Exception(f"{self._valign} is not in t,b,m")
 
-    def __parse_2_top_line(self) -> list[OutputChunk]:
+    def _parse_2_top_line(self) -> list[OutputChunk]:
         return [
             OutputChunk(self._corner_top_left, "frame"),
             OutputChunk(self._width * self._delimiter_top, "frame"),
@@ -88,7 +122,7 @@ class InputCell:
             OutputChunk(self._newline, "end_of_line"),
         ]
 
-    def __parse_3_text_cell(self, line_text: str) -> list[OutputChunk]:
+    def _parse_3_text_cell(self, line_text: str) -> list[OutputChunk]:
         result = [
             OutputChunk(self._delimeter_left, "frame"),
             OutputChunk(self._distance_border_text.left, "cell_align_left"),
@@ -124,29 +158,13 @@ class InputCell:
         result.append(OutputChunk(self._newline, "end_of_line"))
         return result
 
-    def __parse_4_bottom_line(self):
+    def _parse_4_bottom_line(self):
         return [
             OutputChunk(self._corner_bottom_left, "frame"),
             OutputChunk(self._width * self._delimiter_bottom, "frame"),
             OutputChunk(self._corner_bottom_right, "frame"),
             OutputChunk(self._newline, "end_of_line"),
         ]
-
-    def set_cell_text_to_border(self, left: str = " ", right: str = " "):
-        """set the distance to the frame on the text in echt cell\n
-        left = Y = " " \n
-        right = X = " " \n
-        ┌────────────────────┐\n
-        │Y               oneX│\n
-        │Y               oneX│\n
-        └────────────────────┘\n
-        """
-        if not isinstance(left, str) or not isinstance(left, str):
-            raise ValueError(f"left and right must be string!")
-
-        self._distance_border_text = _CellTextDistance(
-            left, right, len(left) + len(right)
-        )
 
     def __check_cell_input(self, cell_text) -> str:
         if not isinstance(cell_text, (str, float, int)):
@@ -160,9 +178,14 @@ class InputCell:
         return cell_text
 
 
-class InputCell_Left(InputCell):
-    def __init__(self, text: str, *args, **kwargs) -> None:
-        super().__init__(text, *args, **kwargs)
+# in a method add_row first check if chars like "+-++ is set" when yes row[0].corner_left = chars[0] row[-1].corner_right = chars[-1]
+class InputCell_Left(CellParser):
+    def __init__(
+        self, text: str, hight: int, width: int = "auto", *args, **kwargs
+    ) -> None:
+        super().__init__(text, hight, width, *args, **kwargs)
+
+    def _init_chars_and_align(self):
         self._corner_top_left = "+"
         self._corner_top_right = "+"
         self._corner_bottom_left = "+"
@@ -171,37 +194,70 @@ class InputCell_Left(InputCell):
         self._delimiter_bottom = "-"
         self._delimeter_left = "|"
         self._delimeter_right = "|"
-        self._width = 20
-        self._hight = 1
-        self._align = "r"
+        self._align = "c"
         self._valign = "t"
         self._newline = ""
 
 
-# row_wrapper = [InputCell("hallo"),InputCell("hallo")]
+class InputCell_Right(CellParser):
+    def __init__(
+        self, text: str, hight: int, width: int = "auto", *args, **kwargs
+    ) -> None:
+        super().__init__(text, hight, width, *args, **kwargs)
 
-cells: list[InputCell] = [InputCell_Left("hallo"), InputCell("hallo")]
-
-# for cell in cells:
-#     for chunk in cell._cell_parser():
-#         print(chunk, end="")
-result = []
-for i in range(6):
-    for cell in cells:
-        result += next(cell._cell_parser())
-        # except
-for chunk in result:
-    print(chunk, end="")
-# chunk_line_0 = next(cells[0]._cell_parser())
-# chunk_line_1 = next(cells[1]._cell_parser())
-# for chunk in chunk_line_0:
-#     print(chunk, end="")
-# for chunk in chunk_line_1:
-#     print(chunk, end="")
+    def _init_chars_and_align(self):
+        self._corner_top_left = ""
+        self._corner_top_right = "+"
+        self._corner_bottom_left = ""
+        self._corner_bottom_right = "+"
+        self._delimiter_top = "-"
+        self._delimiter_bottom = "-"
+        self._delimeter_left = ""
+        self._delimeter_right = "|"
+        self._align = "l"
+        self._valign = "b"
+        self._newline = "\n"
 
 
-# for cell in cells:
-#     chunk_line = cell._cell_parser()
-#     for chunk in chunk_line:
-#         print(chunk, end="")
-# print(c._cell_parser())
+class Colum:
+    def __init__(self, column: list[CellParser]) -> None:
+        self.column = column
+
+
+class Row:
+    def __init__(self, row: list[CellParser]) -> None:
+        self.row = row
+
+    def print(self):
+        while True:
+            try:
+                for cell in self.row:
+                    line = next(cell.get)
+                    for chunk in line:
+                        print(chunk, end="")
+            except StopIteration:
+                break
+
+
+class TextTable:
+    def __init__(self) -> None:
+        self.table = []
+
+    def run(self):
+        for element in self.table:
+            if isinstance(element, Row):
+                pass
+            elif isinstance(element, Colum):
+                pass
+            elif isinstance(element, CellParser):
+                pass
+
+
+c = Row(
+    [
+        InputCell_Left("hehjhuhhhh\nheee", 8),
+        CellParser("hallo", 8),
+        InputCell_Right("hallo", 8),
+    ]
+)
+c.print()
