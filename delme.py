a4d5636e39a046254df64afc98abe30e58f3135f
Philipp Gogolin <gogolin@hm.edu>
2023-11-03 09:03:03 +0100

    Del old code

--- delme.py
+++
@@ -1,9 +0,0 @@
-# specialHline = 5
-
-
-# def get_special_horizontal_border(borders: int | list[int]):
-#     if isinstance(borders, int):
-#         return
-x = None
-lissst = []
-lissst += x
--- validate_val_in_toleance.py
+++
@@ -1,9 +0,0 @@
-class Validator:
-    def __init__(self) -> None:
-        pass
-    def validate(self,actual,tol_err=True,tol_war=True):
-        if tol_err:
-            pass
-
-    def _validate(self,actual,tolerance):
-        if actual is None:
